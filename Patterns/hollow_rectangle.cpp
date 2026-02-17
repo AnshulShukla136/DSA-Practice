4ee5bfd2536e47ba9c58b2320b628193ef2ce78c
Anshul Shukla <152410688+AnshulShukla136@users.noreply.github.com>
2026-02-17 13:32:47 +0530

    Add files via upload

---
+++ Patterns/hollow_rectangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 4, cols = 6;
+    for(int i = 1; i <= rows; i++) {
+        for(int j = 1; j <= cols; j++) {
+            if(i == 1 || i == rows || j == 1 || j == cols)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
